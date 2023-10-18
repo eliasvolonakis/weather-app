2e5b3c4b080a3058d303d71e364a3caefc346be8
Elias Volonakis <eliasvolonakis@Eliass-MacBook-Pro.local>
2023-10-18 10:48:52 -0400

    Enhanced Testing

--- backend/weatherapp/weatherapp/tests.py
+++ backend/weatherapp/weatherapp/tests.py
@@ -12,6 +12,7 @@ from tzfpy import get_tz
 import pytest
 from  rest_framework.test import APIClient
 from .models import WeatherForecast
+import decimal
 
 client = APIClient()
 
@@ -21,6 +22,15 @@ def test_create_forecast():
     assert response.status_code == 200
     forecast = WeatherForecast.objects.first()
     assert forecast is not None
+    assert forecast.date is not None
+    assert type(forecast.date) is str
+    assert forecast.min_temperature is not None
+    assert type(forecast.min_temperature) is decimal.Decimal
+    assert forecast.max_temperature is not None
+    assert type(forecast.max_temperature) is decimal.Decimal
+    assert forecast.precipitation_sum is not None
+    assert type(forecast.precipitation_sum) is decimal.Decimal
+
     
 def test_bad_forecast_request_no_locaton():
     response = client.get('/weather/', {}, follow=True)
