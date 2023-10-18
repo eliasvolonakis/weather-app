561d3606f0096c2c913a8837c35c550384cc8eec
Elias Volonakis <eliasvolonakis@Eliass-MacBook-Pro.local>
2023-10-18 06:44:18 -0400

    Refactored backend logic

--- backend/weatherapp/weatherapp/models.py
+++ backend/weatherapp/weatherapp/models.py
@@ -1,9 +1,9 @@
 from django.db import models
 
 class WeatherForecast(models.Model):
-    date = models.DateField()
+    date = models.CharField(max_length=10)
     min_temperature = models.DecimalField(max_digits=5, decimal_places=2)
     max_temperature = models.DecimalField(max_digits=5, decimal_places=2)
 
     def __str__(self):
-        return f"{self.location} - {self.date}"
+        return f"{self.date}: MIN: {self.min_temperature} MAX: {self.max_temperature}"
--- backend/weatherapp/weatherapp/views.py
+++ backend/weatherapp/weatherapp/views.py
@@ -1,52 +1,53 @@
-from rest_framework.decorators import api_view, renderer_classes
-from rest_framework.renderers import JSONRenderer, TemplateHTMLRenderer
-from django.shortcuts import get_object_or_404
+from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
 import requests
+from tzfpy import get_tz
 from .models import WeatherForecast
+from .serializers import WeatherForecastSerializer
 import os
-from os.path import join, dirname
 from dotenv import load_dotenv
 
 load_dotenv()
-# print("WEATHER_API")
+
 WEATHER_API = os.getenv('WEATHER_API')
-print(WEATHER_API)
 LOCATION_API = os.getenv('LOCATION_API')
-FORECAST_NUM = os.getenv('FORECAST_NUM')
-print(LOCATION_API)
+FORECAST_NUM = int(os.getenv('FORECAST_NUM', 5))  # Default to 5 forecasts
 
-# WEATHER_API = "https://api.open-meteo.com/v1/forecast?hourly=temperature_2m&timezone={}&latitude={}&longitude={}&daily=temperature_2m_max,temperature_2m_min,precipitation_sum"
-# LOCATION_API = "https://geocode.maps.co/search?q={}"
-# location = "Toronto"
-
-print(WEATHER_API)
-
-@api_view(('GET',))
-@renderer_classes((JSONRenderer,))
+@api_view(['GET'])
 def weather_forecast(request, location):
     location_api = LOCATION_API.format(location)
     location_response = requests.get(location_api).json()
+
+    if not location_response:
+        return Response({'error': 'Location not found'}, status=status.HTTP_404_NOT_FOUND)
+
     latitude = location_response[0]['lat']
     longitude = location_response[0]['lon']
-    timezone = "EST"
+    timezone = get_tz(float(longitude), float(latitude))
     weather_api = WEATHER_API.format(timezone, latitude, longitude)
-    weather_response = requests.get(weather_api)
-    print(weather_response)
-    if weather_response.status_code == 200:
-        data = weather_response.json()
-        # Process the data and store it in the database
-        print(data)
-        # Can be changed if more than 5 forecasts are requested
-        i = 0
-        while i < int(FORECAST_NUM):
-            WeatherForecast.objects.create(
-                date = str(data["daily"]["time"][i]),
-                min_temperature = data["daily"]['temperature_2m_min'][i],
-                max_temperature = data["daily"]['temperature_2m_max'][i],
-            )
-            i += 1
-        return Response(data, status=status.HTTP_200_OK)
-    else:
+
+    try:
+        weather_response = requests.get(weather_api)
+
+        if weather_response.status_code == 200:
+            data = weather_response.json()
+
+            forecasts = []
+
+            for i in range(min(FORECAST_NUM, len(data["daily"]["time"]))):
+                w = WeatherForecast(
+                    date=data["daily"]["time"][i],
+                    min_temperature=data["daily"]['temperature_2m_min'][i],
+                    max_temperature=data["daily"]['temperature_2m_max'][i],
+                )
+                w.save()  # Save the forecast to the database
+                serializer = WeatherForecastSerializer(w)  # Serialize the forecast
+                forecasts.append(serializer.data)
+
+            return Response(forecasts, status=status.HTTP_200_OK)
+
         return Response({'error': 'Failed to fetch data from the weather API'}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+    except requests.exceptions.RequestException as e:
+        return Response({'error': 'Failed to fetch data from the weather API: ' + str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
