d3b72b115b745d653ec80ae94a5e1eb2cebddf55
Elias Volonakis <eliasvolonakis@Eliass-MacBook-Pro.local>
2023-10-18 14:47:25 -0400

    Added Background Image and Table

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import WeatherForecast from './components/WeatherForecast';
+
+const appStyle= {
+  width: '100vw',
+  height: '100vh',
+  backgroundImage: `url(${'./../images/sky.png'})`,
+  backgroundPosition: 'center',
+  backgroundSize: 'cover',
+  backgroundRepeat: 'no-repeat',
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={appStyle}>
+      <body>
+        <WeatherForecast />
+      </body>
     </div>
   );
 }
---
+++ frontend/src/components/WeatherForecast.js
@@ -0,0 +1,105 @@
+import React, { useState } from 'react';
+import './../styles/WeatherForecast.css';
+
+function WeatherForecast() {
+  const [location, setLocation] = useState(''); // Use an empty string as the initial location
+  const [weatherData, setWeatherData] = useState([]);
+  const [error, setError] = useState(null);
+
+  const handleGenerateForecast = () => {
+    fetchData(); // Trigger fetching data when the button is clicked
+  };
+
+  const fetchData = async () => {
+    try {
+      const response = await fetch(`http://localhost:8000/weather/${location}`);
+      if (response.ok) {
+        const data = await response.json();
+        setWeatherData(data);
+        setError(null); // Clear any previous errors
+      } else {
+        setError('Failed to fetch weather data.');
+      }
+    } catch (error) {
+      setError('An error occurred while fetching weather data.');
+    }
+  };
+
+  const renderTable = () => {
+    // Group weather data by date
+    const groupedData = {};
+    weatherData.forEach((forecast) => {
+      const date = forecast.date;
+      if (!groupedData[date]) {
+        groupedData[date] = {
+          min_temperature: [],
+          max_temperature: [],
+          precipitation_sum: [],
+        };
+      }
+      groupedData[date].min_temperature.push(forecast.min_temperature);
+      groupedData[date].max_temperature.push(forecast.max_temperature);
+      groupedData[date].precipitation_sum.push(forecast.precipitation_sum);
+    });
+
+    // Get unique dates for columns
+    const dates = Object.keys(groupedData);
+
+    // Render the table
+    return (
+      <table className="forecast-table">
+        <thead>
+          <tr>
+            <th>Date</th>
+            {dates.map((date) => (
+              <th key={date}>{date}</th>
+            ))}
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Min Temperature</td>
+            {dates.map((date) => (
+              <td key={date}>
+                {groupedData[date].min_temperature.join(', ')}
+              </td>
+            ))}
+          </tr>
+          <tr>
+            <td>Max Temperature</td>
+            {dates.map((date) => (
+              <td key={date}>
+                {groupedData[date].max_temperature.join(', ')}
+              </td>
+            ))}
+          </tr>
+          <tr>
+            <td>Precipitation Sum</td>
+            {dates.map((date) => (
+              <td key={date}>
+                {groupedData[date].precipitation_sum.join(', ')}
+              </td>
+            ))}
+          </tr>
+        </tbody>
+      </table>
+    );
+  };
+
+  return (
+    <div className="WeatherForecast">
+      <h2>Weather Forecast</h2>
+      <input
+        type="text"
+        placeholder="Enter location"
+        value={location}
+        onChange={(e) => setLocation(e.target.value)}
+      />
+      <button onClick={handleGenerateForecast}>Generate Forecast</button>
+      {error && <p className="error">{error}</p>}
+      <div className="table-container">{renderTable()}</div>
+    </div>
+  );
+}
+
+export default WeatherForecast;
