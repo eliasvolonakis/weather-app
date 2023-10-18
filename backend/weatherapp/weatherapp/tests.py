e89a520c47858cd9f03d9b73bc33eb3dbf1558d6
Elias Volonakis <eliasvolonakis@Eliass-MacBook-Pro.local>
2023-10-18 10:50:05 -0400

    Removed Uneeded Imports from Tests

--- backend/weatherapp/weatherapp/tests.py
+++ backend/weatherapp/weatherapp/tests.py
@@ -1,14 +1,3 @@
-from django.test import TestCase
-from rest_framework.test import APIClient
-from rest_framework import status
-from .models import WeatherForecast
-from unittest.mock import patch, Mock
-import json
-from rest_framework.decorators import api_view
-from rest_framework.response import Response
-from rest_framework import status
-import requests
-from tzfpy import get_tz
 import pytest
 from  rest_framework.test import APIClient
 from .models import WeatherForecast
